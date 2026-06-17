2d0d82d7c3d168e29f34982d6e3ba7ebdfdb773f
Zeinab <z.jorkesh1989@gmail.com>
2026-06-17 22:52:31 +0800

    Added weather icon from API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 function formatDate (timestamp){
     let date = new Date (timestamp);
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Fraiday","Saturday"]
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Fraiday","Saturday"];
     let day = days[date.getDay()];
     let hours = date.getHours();
+    if (hours<10){
+        hours= `0${hours}`
+    }
     let minutes = date.getMinutes();
+    if (minutes<10){
+        minutes= `0${minutes}`
+    }
     return `${day}, ${hours}:${minutes}`;
     console.log(timestamp);
 
@@ -18,6 +24,7 @@ function displayTemperature(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind-speed");
+    let iconElement = document.querySelector("#icon");
 
     dateElement.innerHTML = formatDate(response.data.time*1000);
     cityElement.innerHTML = response.data.city;
@@ -25,13 +32,14 @@ function displayTemperature(response){
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
     
 
 }
 
 
 let apiKey = "8f8ba35f23t75fbc75d7do5424f8040b";
-let city = "sydney";
+let city = "toronto";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 
