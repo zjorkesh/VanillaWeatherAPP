14c84f3ced3be6a67445defd69a2b0f685424ea1
Zeinab <z.jorkesh1989@gmail.com>
2026-06-25 00:37:27 +0800

    Added forecasting data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function formatDate (timestamp){
     let date = new Date (timestamp);
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Fraiday","Saturday"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let day = days[date.getDay()];
     let hours = date.getHours();
     if (hours<10){
@@ -36,12 +36,24 @@ function displayTemperature(response){
     windElement.innerHTML = Math.round(response.data.wind.speed);
     iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
     iconElement.setAttribute ("alt", response.data.condition.description);
+
+    getForecast(
+        response.data.coordinates.latitude,
+        response.data.coordinates.longitude
+    );
+}
+
+function getForecast(lat,lon){
+    let apiKey = "8f8ba35f23t75fbc75d7do5424f8040b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function search (city){
     let apiKey = "8f8ba35f23t75fbc75d7do5424f8040b";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(displayTemperature);
+
 }
 
 function handleSubmit(event){
@@ -78,8 +90,46 @@ function showCelsiusTemperature (event){
      feelsLikeElement.innerHTML = Math.round(celsiusFeelsLike);
 }
 
+function displayForecast(response){
+    
+    forecastData = response.data.daily;
+    let forecast = response.data.daily;
+
+    let forecastHTML = "";
+
+    forecast.forEach(function(day, index){
+        if (index < 6){
+            forecastHTML = forecastHTML + 
+            `
+            <div>
+             <div>${formatDay(day.time)}</div>
+             <img src="${day.condition.icon_url}" alt="">
+             <div>
+              ${Math.round(day.temperature.maximum)}°
+              ${Math.round(day.temperature.minimum)}°
+             </div>
+            </div>
+
+            `;
+        }
+
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 let celsiusTemperature = null;
 let celsiusFeelLike = null;
+let forecastData = null;
 
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
@@ -87,5 +137,8 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+
+
 
 search("Stockholm");
